4e386777960b44db79f53aa02b143518f03693d3
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-01-06 12:10:17 -0800

    Add controls disabler function to menu script

--- storegameURP/Assets/Scripts/Menu.cs
+++ storegameURP/Assets/Scripts/Menu.cs
@@ -13,5 +13,7 @@ public abstract class Menu : MonoBehaviour
         MenuActions.Exit.performed += _ => Open(false);
     }
 
+    void OnDisable() => controls.Disable();
+
     public virtual void Open(bool value) => gameObject.SetActive(value);
 }
