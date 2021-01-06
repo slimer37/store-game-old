8badc76fc0d3f9542985e48e2adbaf9e72907041
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-01-06 12:10:00 -0800

    Steal loading screen script

--- storegameURP/Assets/Scripts/SceneLoader.cs
+++ storegameURP/Assets/Scripts/SceneLoader.cs
@@ -1,7 +1,38 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene(int index) => SceneManager.LoadScene(index);
+    // Thanks, Brackeys!
+    public static SceneLoader Current { get; private set; }
+
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private Slider loadingBar;
+    [SerializeField] private TextMeshProUGUI progressText;
+
+    void Awake() => Current = this;
+
+    public void LoadScene(int sceneIndex)
+    {
+        Time.timeScale = 1.0f;
+        StartCoroutine(LoadAsync(sceneIndex));
+    }
+
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            loadingBar.value = progress;
+            progressText.text = Mathf.RoundToInt(progress * 100f) + "%";
+            yield return null;
+        }
+    }
 }
