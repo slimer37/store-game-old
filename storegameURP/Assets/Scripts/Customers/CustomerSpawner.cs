37a747238df2c9465263c8b3f500168242779e98
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-01-09 19:38:34 -0800

    Fix spawn chance math

--- storegameURP/Assets/Scripts/Customers/CustomerSpawner.cs
+++ storegameURP/Assets/Scripts/Customers/CustomerSpawner.cs
@@ -6,7 +6,7 @@ public class CustomerSpawner : MonoBehaviour
     [SerializeField] private Transform[] endPositions;
     [SerializeField] private GameObject customerPrefab;
     [SerializeField] private float startDelay;
-    [SerializeField, Range(0, 60)] private int customersPerMinute;
+    [SerializeField, Range(1, 60)] private int customersPerMinute;
 
     private static int customers;
 
@@ -26,7 +26,7 @@ public class CustomerSpawner : MonoBehaviour
             }
 
             // If there is at least one product available, take a random chance based on customer density.
-            if (Product.ProductsAvailable.Count >= 1 && Random.Range(0, 100) <= (customersPerMinute / 60) * 100)
+            if (Product.ProductsAvailable.Count >= 1 && Random.Range(0, 101) <= (float)customersPerMinute / 60 * 100)
             {
                 Customer customer = Instantiate(customerPrefab, transform.position, Quaternion.identity).GetComponent<Customer>();
                 customer.Wanted = Product.GetRandomProduct();
