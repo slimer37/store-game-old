98a36433a5e0442f6dcc4cd94a60202dbdbe270c
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-01-10 19:28:40 -0800

    Add escape as menu exit binding
    Needed to create function for Open(false) to be able to unsubscribe it from the exit key in the pause menu script. Pause menu also does not open if there's already a menu open (only the Inspect graphic as of now).

--- storegameURP/Assets/Scripts/UI/PauseMenu.cs
+++ storegameURP/Assets/Scripts/UI/PauseMenu.cs
@@ -9,15 +9,20 @@ public class PauseMenu : Menu
     protected override void Awake()
     {
         base.Awake();
-        MenuActions.Pause.performed += _ => Open(!transform.GetChild(0).gameObject.activeSelf);
+        MenuActions.Exit.performed -= Exit;
+        MenuActions.Pause.performed += _ => Open(!open);
     }
 
     public override void Open(bool value)
     {
-        (value ? OnPause : OnResume).Invoke();
-        transform.GetChild(0).gameObject.SetActive(value);
-        Time.timeScale = value ? 0 : 1;
-        Cursor.visible = value;
-        Cursor.lockState = value ? CursorLockMode.None : CursorLockMode.Locked;
+        if (!MenuManager.MenuOpen)
+        {
+            open = value;
+            (value ? OnPause : OnResume).Invoke();
+            transform.GetChild(0).gameObject.SetActive(value);
+            Time.timeScale = value ? 0 : 1;
+            Cursor.visible = value;
+            Cursor.lockState = value ? CursorLockMode.None : CursorLockMode.Locked;
+        }
     }
 }
