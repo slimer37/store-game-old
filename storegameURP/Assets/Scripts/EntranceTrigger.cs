cb30d9b80eeb9fa7f3148046998d291fa2c2b113
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-01-17 12:49:12 -0800

    Add option to EntranceTrigger for player to activate it

--- storegameURP/Assets/Scripts/EntranceTrigger.cs
+++ storegameURP/Assets/Scripts/EntranceTrigger.cs
@@ -5,15 +5,17 @@ public class EntranceTrigger : MonoBehaviour
 {
     public System.Action<bool> OnCustomerTrigger;
 
+    [SerializeField] private bool playerTriggers;
+
     void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Customer"))
+        if (other.CompareTag("Customer") || playerTriggers && other.CompareTag("Player"))
         { OnCustomerTrigger?.Invoke(true); }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Customer"))
+        if (other.CompareTag("Customer") || playerTriggers && other.CompareTag("Player"))
         { OnCustomerTrigger?.Invoke(false); }
     }
 }
