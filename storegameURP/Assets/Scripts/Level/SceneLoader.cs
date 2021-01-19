2d32cf950a6855287e869da7423cfba6d0d58b05
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-01-18 22:19:58 -0800

    Add progress captions to loading screen

--- storegameURP/Assets/Scripts/Level/SceneLoader.cs
+++ storegameURP/Assets/Scripts/Level/SceneLoader.cs
@@ -12,6 +12,8 @@ public class SceneLoader : MonoBehaviour
     [SerializeField] private GameObject loadingScreen;
     [SerializeField] private Slider loadingBar;
     [SerializeField] private TextMeshProUGUI progressText;
+    [SerializeField] private TextMeshProUGUI progressCaption;
+    [SerializeField] private string[] progressCaptions;
 
     void Awake() => Current = this;
 
@@ -32,6 +34,10 @@ public class SceneLoader : MonoBehaviour
             float progress = Mathf.Clamp01(operation.progress / 0.9f);
             loadingBar.value = progress;
             progressText.text = Mathf.RoundToInt(progress * 100f) + "%";
+
+            var captionIndex = Mathf.RoundToInt(progress * progressCaptions.Length);
+            progressCaption.text = progressCaptions[Mathf.Clamp(captionIndex, 0, progressCaptions.Length)];
+
             yield return null;
         }
     }
