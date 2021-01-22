862c8d2ded639d266838fb0f2b1ed4e756517efd
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-01-21 16:00:44 -0800

    Create channel surveillance system with monitors and night vision

---
+++ storegameURP/Assets/Scripts/Monitor.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using TMPro;
+
+public class Monitor : Interactable
+{
+    protected override CursorIcon.Icon HoverIcon => CursorIcon.Icon.Access;
+
+    [SerializeField] private Renderer rend;
+    [SerializeField] private TextMeshPro channelNumText;
+    [SerializeField] private TextMeshPro angleText;
+
+    private bool useNightVision = false;
+    private int channel = 0;
+
+    private Surveil currentCam => Surveil.AllCameras[channel];
+
+    void Start()
+    {
+        UpdateScreen();
+        UpdateText();
+    }
+
+    public override void Interact() => ChangeChannel(1);
+    public override void SecondaryInteract() => ChangeChannel(-1);
+
+    public void ToggleNightVision()
+    {
+        useNightVision = !useNightVision;
+        currentCam.UseNightVision(useNightVision);
+        UpdateText();
+    }
+
+    void ChangeChannel(int amount)
+    {
+        channel = (Surveil.AllCameras.Count + channel + amount) % Surveil.AllCameras.Count;
+        currentCam.UseNightVision(useNightVision);
+        UpdateScreen();
+        UpdateText();
+    }
+
+    void UpdateText() => channelNumText.text = $"CH {channel} <b><size=50%>NV:<color={(useNightVision ? "green>ON" : "yellow>OFF")}";
+
+    void UpdateScreen()
+    {
+        rend.material.SetTexture("_BaseMap", currentCam.Texture);
+        rend.material.SetTexture("_EmissionMap", currentCam.Texture);
+    }
+
+    void Update() => angleText.text = $"{currentCam.transform.localEulerAngles.y:000}°";
+}
--- storegameURP/Assets/Scripts/Surveil.cs
+++ storegameURP/Assets/Scripts/Surveil.cs
@@ -1,11 +1,37 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Surveil : MonoBehaviour
 {
     [SerializeField] private float turnInterval;
     [SerializeField] private float turnDegrees;
     [SerializeField] private float turnTime;
+
+    [Header("Rendering")]
+    [SerializeField] private Camera cam;
+    [SerializeField] private Volume volume;
+
+    [field: Header("Night Vision")]
+    [field: SerializeField] public VolumeProfile NightVisionProfile { get; private set; }
+
+    private VolumeProfile defaultProfile;
+
+    public RenderTexture Texture { get; private set; }
+    public static List<Surveil> AllCameras { get; private set; } = new List<Surveil>();
+
+    void OnDestroy() => AllCameras.Remove(this);
+
+    void Awake()
+    {
+        AllCameras.Add(this);
+        defaultProfile = volume.sharedProfile;
+        Texture = new RenderTexture(512, 256, 24);
+        cam.targetTexture = Texture;
+    }
+
+    public void UseNightVision(bool value) => volume.sharedProfile = value ? NightVisionProfile : defaultProfile;
 
     IEnumerator Start()
     {
